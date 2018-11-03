b9f804d8e74913a9a277e9865032c7b1150bb938
niutim <timothe.coger@gmail.com>
2018-11-03 14:29:33 +0100

    Datastore - Ajout structure pour EF 6

---
+++ Data/Adapters/CharacterAdapter.cs
@@ -0,0 +1,19 @@
+﻿using PokemonShared.Models;
+using System.Collections.Generic;
+
+namespace PokemonDataStore.Adapters
+{
+    public class CharacterAdapter
+    {
+        public List<Character> GetCharacter()
+        {
+            List<Character> charactersList = new List<Character>();
+
+            using (Entity.PoketMonstersDB pmDB = new Entity.PoketMonstersDB())
+            {
+            }
+
+            return charactersList;
+        }
+    }
+}
